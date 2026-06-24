083f3c34464b00e727934720c501009317c50957
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-06-24 18:29:16 +0200

    refactor: remove unused import in Helper class

--- backend/src/main/java/za/ac/cput/guiltfreecookie/util/Helper.java
+++ backend/src/main/java/za/ac/cput/guiltfreecookie/util/Helper.java
@@ -1,6 +1,6 @@
 package za.ac.cput.guiltfreecookie.util;
 
-import za.ac.cput.guiltfreecookie.domain.CookieCategory;
+
 
 public class Helper {
     public static boolean isNullOrEmpty(String str) {
